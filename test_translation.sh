b24580dbf3532bdee55a6aad50c3e7e533fa0941
wangli <freelw81@qq.com>
2025-04-02 16:23:31 +0800

    update

--- test_translation.sh
+++ test_translation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 set -e
+pushd ./checkpoints/save/
 cat checkpoint_20250402_150847_40_part_aa checkpoint_20250402_150847_40_part_ab > checkpoint_20250402_150847_40.bin
-./transformer -e 0 -c ./checkpoint_20250402_150847_40.bin
+popd
+./transformer -e 0 -c ./checkpoints/save/checkpoint_20250402_150847_40.bin
